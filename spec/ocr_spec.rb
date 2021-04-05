44aacc01eb13ff3a1c9d3a428fb6a2aae90995ce
Meagan Waller <meaganewaller@gmail.com>
2021-04-05 11:26:20 -0400

    :sparkles: Parse every number individually, setup failing spec to parse multiple numbers at once

--- spec/ocr_spec.rb
+++ spec/ocr_spec.rb
@@ -2,8 +2,15 @@ require 'spec_helper'
 require './ocr_reader'
 
 describe "OCR Reader" do
+  (0..9).each do |i|
+    it "returns #{i}" do
+      ocr_reader = OcrReader.new("./spec/fixtures/#{i}.txt")
+      expect(ocr_reader.parse).to eql i
+    end
+  end
+
   it "returns 000000000" do
-    ocr_reader = OcrReader.new('./user_stories/story1.txt')
-    expect(ocr_reader.parse).to eql 0
+    ocr_reader = OcrReader.new("./spec/fixtures/000000000.txt")
+    expect(ocr_reader.parse).to eql 000000000
   end
 end
